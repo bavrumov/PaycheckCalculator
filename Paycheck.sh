5a255209980382c8980223bd91b5e60436adc514
Boris Avrumov <bavrumov@hotmail.com>
2019-03-22 00:00:57 -0400

    Shorter message

--- Paycheck.sh
+++ Paycheck.sh
@@ -3,13 +3,13 @@
 
 if [ $# -eq 1 ]
 then
-	zip="10055" # Defaults to a NYC zip-code in midtown
+	zip="10055" # Defaults to a Midtown Manhattan Zip code if missing second parameter
 else
 	zip=$2
 fi
 
 curl_command="curl --silent -d 'ud-current-location=ZIP|${zip}&ud-pay-fq=8&ud-it-household-income=${1}' -X POST https://smartasset.com/taxes/new-york-paycheck-calculator?render=json&"
 
-echo "Your estimated annual take home pay after earning \$$1 in zip code $zip is... "
-income=$(eval $curl_command | jq '.page_data.annualizedPaycheck' )
+echo "Your estimated annual take home pay after earning \$$1 in zip $zip is..."
+income=$(eval $curl_command | jq '.page_data.annualizedPaycheck')
 echo "($income+0.5)/1" | bc
